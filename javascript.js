c67a7ba6ddf1b4da318483020aa41e97d3560fcb
KSuzuki <98202109+kbelltree@users.noreply.github.com>
2023-02-15 17:12:54 -0800

    Add a button that generates grids per user's entry

--- javascript.js
+++ javascript.js
@@ -1,32 +1,77 @@
 const container = document.getElementById("container");
-// create 256 div's in DOM (for 16 x 16 square grid)
-function createGridCell(){
+const resetButton = document.getElementById("reset");
+const bnwButton = document.getElementById("bnw");
+ 
+// Part 1. create 256 div's in DOM (for 16 x 16 square grid)
+// Part 2. create the number of div's per the prompt entry 
+function createGridCell(divCountToSquare){
     const fragment = document.createDocumentFragment();
-    for (let i = 0; i < 256; i++) {
+    // for (let i = 0; i < 256; i++) {
+    let doubled = divCountToSquare ** 2; 
+    for (let i = 0; i < doubled; i++){
         let cell = document.createElement("div")
         cell.className = "cell";
         fragment.append(cell);
     }
   container.append(fragment);
 }
-// create a function that creates 16 x 16 square grid in a container
-function createGrid() {
-    // container.style.width = "100%";
-    container.style.gridTemplateColumns = "repeat(16, 1fr)";
-    container.style.gridTemplateRows = "repeat(16, 1fr)";
+// Part 1. Function that creates 16 x 16 square grid in a container
+// Part 2. Function that creates grid per the prompt entry 
+function createGrid(cellCountPerSide) {
+    // container.style.gridTemplateColumns = "repeat(16, 1fr)";
+    // container.style.gridTemplateRows = "repeat(16, 1fr)";
+    container.style.gridTemplateColumns = `repeat(${cellCountPerSide}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${cellCountPerSide}, 1fr)`
 }
-createGridCell();
-createGrid();
+// createGridCell();
+// createGrid();
 
-// create a function that colors the cell on click and hover
+// Function that colors the cell on hover
 function addColor() {
     // DOM style.backgroundColor 
     this.style.cursor = "pointer";
     this.style.backgroundColor = "black";
 }  
 
-// Event listener that fires on mouseenter
-const gridItems = document.querySelectorAll(".cell");
-gridItems.forEach(function(item){
-    item.addEventListener("mouseenter", addColor);
-})
+// Function that prompt the user to enter an integer between 1 and 100
+function getUserInput() {
+    let enteredValue = prompt("Enter an integer between 1 and 100", 1);
+    if (enteredValue === null || enteredValue === "") {
+        console.log(enteredValue + " falsy"); 
+        return; 
+    } else if (!Number.isInteger(+enteredValue)|| isNaN(+enteredValue) || +enteredValue < 1 || +enteredValue > 100){
+        console.log(enteredValue + " also falsy");
+        alert("The number has to be an integer between 1 and 100.");
+        getUserInput();
+        return; 
+    } else if (+enteredValue >= 1 && +enteredValue <= 100){
+        console.log(enteredValue + " truthy");
+        return +enteredValue; 
+    } else {
+        return; 
+    }
+}
+
+// getUserInput();
+
+// Function that creates div's and grid cells per the user's entry
+function setUpGrid(){
+    refreshCanvas();
+    let userEntry = getUserInput(); 
+    createGridCell(userEntry);
+    createGrid(userEntry);
+    const gridItems = document.querySelectorAll(".cell"); 
+    gridItems.forEach(function(item){
+        item.addEventListener("mouseenter", addColor);
+    })
+}
+
+// Function that removes the previous div elements in container
+function refreshCanvas(){
+    while (container.firstChild) 
+        container.removeChild(container.firstChild);
+}
+
+// Event listener attached to reset button 
+resetButton.addEventListener("click", setUpGrid);
+
